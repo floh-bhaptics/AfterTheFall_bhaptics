34468116818a3aa82f70fa94f0d0507523ebb890
floh-bhaptics <floh@fahrenbergers.de>
2023-03-08 19:24:52 +0100

    Added player damage

--- AfterTheFall_bhaptics.cs
+++ AfterTheFall_bhaptics.cs
@@ -8,7 +8,11 @@ using BepInEx.Logging;
 using HarmonyLib;
 using MyBhapticsTactsuit;
 using Snowbreed.Client;
+using UnityEngine;
+using Vertigo.PlayerBody;
+using Vertigo.Snowbreed;
 using Vertigo.Snowbreed.Client;
+using static AfterTheFall_bhaptics.Plugin;
 
 namespace AfterTheFall_bhaptics
 {
@@ -31,12 +35,31 @@ namespace AfterTheFall_bhaptics
         }
 
         [HarmonyPatch(typeof(Gun), "FireBullet", new Type[] { typeof(bool), typeof(bool) })]
-        public class StartGliding
+        public class FireGun
         {
             public static void Postfix(Gun __instance)
             {
                 bool isRight = (__instance.MainHandSide == Vertigo.VR.EHandSide.Right);
                 tactsuitVr.ShootRecoil(isRight);
+            }
+        }
+
+        [HarmonyPatch(typeof(PlayerBodyHit), "Apply", new Type[] { typeof(PlayerBodyBehaviour), typeof(Quaternion), typeof(Vertigo.VertigoAnimation.IK.FBBIKJobData) })]
+        public class PlayerHit
+        {
+            public static void Postfix(PlayerBodyHit __instance)
+            {
+                tactsuitVr.LOG("Direction: " + __instance.localHeadHitDirection.x.ToString() + " " + __instance.localHeadHitDirection.z.ToString());
+                Vector3 hitPosition = __instance.localHeadHitDirection;
+                Vector3 flattenedHit = new Vector3(hitPosition.x, 0f, hitPosition.z);
+                Vector3 patternOrigin = new Vector3(0f, 0f, 1f);
+                float earlyhitAngle = Vector3.Angle(flattenedHit, patternOrigin);
+                Vector3 earlycrossProduct = Vector3.Cross(flattenedHit, patternOrigin);
+                if (earlycrossProduct.y > 0f) { earlyhitAngle *= -1f; }
+                float myRotation = earlyhitAngle; //- playerDir.y;
+                myRotation *= -1f;
+                if (myRotation < 0f) { myRotation = 360f + myRotation; }
+                tactsuitVr.PlayBackHit("Slash", myRotation, 0.0f);
             }
         }
 
